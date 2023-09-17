67fd31f0dba4079caf61c213c241afe1b365732a
3bd alsalam <mahmoudeidabdelsalam89@gmail.com>
2023-09-18 00:50:12 +0300

    edit api

--- inc/wp-functions.php
+++ inc/wp-functions.php
@@ -469,3 +469,8 @@ function additionCarManually($id_basic, $car_name, $car_price, $car_price_after,
     }
   return $car_id;
 }
+
+function formatPost(&$post){
+  unset($post->post_title, $post->post_author, $post->post_date, $post->post_modified, $post->post_date_gmt, $post->post_content, $post->comment_status, $post->ping_status, $post->post_password, $post->to_ping, $post->pinged, $post->post_modified_gmt, $post->post_content_filtered, $post->post_parent, $post->guid, $post->post_mime_type);
+  unset($post->comment_count, $post->comment_count, $post->filter, $post->menu_order, $post->post_status);
+}
